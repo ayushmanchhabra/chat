9066216990dd2462dea0c2b82aa29931898a0f87
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-09-07 21:48:24 +0530

    chore: rename client to web dir

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
+import https from 'node:https';
+
 import express from 'express';
 
 const app = express();
+
+const hostname = import.meta.env.HOSTNAME;
+const port = import.meta.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log(`Server is running on ${hostname}:${port}`);
 });
